32d8bbb9e97eee6d9d69a0d3aefb7b61106a9484
alirezaghey <alireza.q1357@gmail.com>
2021-12-10 17:15:54 +0100

    solve Error exercise

--- exercises/error_handling/errors4.rs
+++ exercises/error_handling/errors4.rs
@@ -1,8 +1,6 @@
 // errors4.rs
 // Make this test pass! Execute `rustlings hint errors4` for hints :)
 
-// I AM NOT DONE
-
 #[derive(PartialEq, Debug)]
 struct PositiveNonzeroInteger(u64);
 
@@ -13,9 +11,26 @@ enum CreationError {
 }
 
 impl PositiveNonzeroInteger {
+    // Using match construct
     fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
-        Ok(PositiveNonzeroInteger(value as u64))
+        match value {
+            // Below line is a way to match infinite/open ranges
+            // i64::MIN..=-1_i64 => Err(CreationError::Negative),
+            v if { v < 0 } => Err(CreationError::Negative),
+            0 => Err(CreationError::Zero),
+            _ => Ok(PositiveNonzeroInteger(value as u64)),
+        }
     }
+    // Using standard if else statements
+    // fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
+    //     if value < 0 {
+    //         Err(CreationError::Negative)
+    //     } else if value == 0 {
+    //         Err(CreationError::Zero)
+    //     } else {
+    //     Ok(PositiveNonzeroInteger(value as u64))
+    //     }
+    // }
 }
 
 #[test]
